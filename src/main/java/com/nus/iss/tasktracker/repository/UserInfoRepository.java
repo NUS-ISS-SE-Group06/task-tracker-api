ddca101d2374a576934f55d12a8778bf194481fd
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-02-20 22:23:58 +0800

    Compilation Error Fix
    * Commented code in UserInfoRepository to resolve Compilation Error

--- src/main/java/com/nus/iss/tasktracker/repository/UserInfoRepository.java
+++ src/main/java/com/nus/iss/tasktracker/repository/UserInfoRepository.java
@@ -1,6 +1,6 @@
 package com.nus.iss.tasktracker.repository;
 
-import com.nus.iss.tasktracker.model.UserInfo;
+//import com.nus.iss.tasktracker.model.UserInfo;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,15 +9,18 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+// FIXME - TO ADD THE CODE BACK ONCE THE MODEL AND RELEVANT CODE IS WRITTEN
 @Repository
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-
-    boolean existsByUserName(String name);
-
-    Optional<UserInfo> findByUserIdAndDeletedFlag(Integer id, boolean isDeletedFlag);
-    List<UserInfo> findByDeletedFlagFalse();
-    @Query("SELECT COALESCE(max(a.userId),0) from UserInfo a")
-    @Transactional
-    Integer findMaxId();
+public interface UserInfoRepository
+//        extends JpaRepository<UserInfo, Integer>
+        {
+//
+//    boolean existsByUserName(String name);
+//
+//    Optional<UserInfo> findByUserIdAndDeletedFlag(Integer id, boolean isDeletedFlag);
+//    List<UserInfo> findByDeletedFlagFalse();
+//    @Query("SELECT COALESCE(max(a.userId),0) from UserInfo a")
+//    @Transactional
+//    Integer findMaxId();
 
 }
